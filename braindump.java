c8814dba17b9374e2c4f6897e54486c8363bdf01
kisrael02 <72632676+kisrael02@users.noreply.github.com>
2020-10-10 12:05:07 -0400

    Update braindump.java

--- braindump.java
+++ braindump.java
@@ -6,6 +6,31 @@ class main
   int n;
   ArrayList<String> brainDump = new ArrayList<String>;
 
+    void menu()
+  {
+      System.out.println("Menu");
+      System.out.println("1. Brain dump");
+      System.out.println("2. Display brain dump");
+      System.out.println("3. Reorganize");
+      System.out.println("Please make your choice here");
+      n = Integer.parseInt(Scanner.nextInt());
+      switch(n)
+      {
+          case 1:
+            addData();
+            break;
+          case 2:
+            display();
+            break;
+  //add reorganize stuff
+          case 3:
+            break;
+          default:
+            System.out.println("Please make a valid choice.");
+            menu();
+          
+      }
+  
   void addData()
   {
     System.out.println("Please enter how many ideas you have.");
