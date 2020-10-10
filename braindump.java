b6030bb8d38edfa6cad6d161ad6ce9604ddbfba5
kisrael02 <72632676+kisrael02@users.noreply.github.com>
2020-10-10 11:39:58 -0400

    Add files via upload

---
+++ braindump.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+
+class brainDump
+{
+  int n;
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> brainDump = new ArrayList<String>;
+        System.out.println("Please enter how many ideas you have.");
+        n = Integer.parseInt(Scanner.nextInt());
+        System.out.println("");
+    }
+}
